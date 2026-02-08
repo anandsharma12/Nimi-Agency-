66fc4bea83b04ae02c85c555b06f04de18f58d81
jayantkhurana2020 <khuranajayant2020@gmail.com>
2026-02-08 14:49:25 +0530

    updated course grid

--- src/data/courses.js
+++ src/data/courses.js
@@ -13,7 +13,7 @@ export const courses = [
     subtitle: "(5x Clicks)",
     price: 52,
     soldOut: true,
-    image: "https://d2j6dbq0eux0bg.cloudfront.net/images/116421013/4917230584.jpg"
+    image: "https://d2j6dbq0eux0bg.cloudfront.net/images/116421013/5352554432.jpg"
   },
   {
     id: 3,
@@ -21,6 +21,6 @@ export const courses = [
     subtitle: "[All Incl]",
     price: 52,
     soldOut: true,
-    image: "https://d2j6dbq0eux0bg.cloudfront.net/images/116421013/4917230584.jpg"
+    image: "https://d2j6dbq0eux0bg.cloudfront.net/images/116421013/products/808805484/5519216124.jpg"
   }
 ];
